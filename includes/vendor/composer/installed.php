54cdc660a9371337df6234cdf80b37afe517ac12
agileangad <ritikesh.ac@gmail.com>
2023-11-23 15:25:31 +0530

    initialtest

---
+++ includes/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.7.x-dev',
+        'version' => '1.7.9999999.9999999-dev',
+        'reference' => '44a493171a60e16603daf81c5d1f0d576b6262b5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.7.x-dev',
+            'version' => '1.7.9999999.9999999-dev',
+            'reference' => '44a493171a60e16603daf81c5d1f0d576b6262b5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'erusev/parsedown' => array(
+            'pretty_version' => '1.7.4',
+            'version' => '1.7.4.0',
+            'reference' => 'cb17b6477dfff935958ba01325f2e8a2bfa6dab3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../erusev/parsedown',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ includes/version.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace mytinytodo;
+
+class Version
+{
+    const VERSION = '1.7.6';
+    const DB_VERSION = '1.7';
+}
